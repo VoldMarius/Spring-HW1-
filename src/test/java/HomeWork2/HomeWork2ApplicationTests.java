f218928c871cb81219c3fafe72a94566bea7a720
VoldMarius <ukusil@mail.ru>
2024-02-12 12:40:33 +0300

    Create HomeWork2

---
+++ src/test/java/HomeWork2/HomeWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
