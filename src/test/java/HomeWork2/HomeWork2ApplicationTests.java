c6507318491bad924fc6f88dc3d50344a1ebc3a1
VoldMarius <ukusil@mail.ru>
2024-02-12 12:54:08 +0300

    Copy all from seminar

--- src/test/java/HomeWork2/HomeWork2ApplicationTests.java
+++ src/test/java/HomeWork2/HomeWork2ApplicationTests.java
@@ -1,13 +1,14 @@
 package HomeWork2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HomeWork2ApplicationTests {
-
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork2ApplicationTests.class, args);
+	}
 	}
 
-}
+
